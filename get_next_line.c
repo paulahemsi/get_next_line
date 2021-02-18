67c70aee6729392815356adbe5bd5ef23bc759db
paulahemsi <paulahemsi@gmail.com>
2021-02-18 20:24:36 -0300

    começou a funcionar, problema com o buffer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 12:17:28 by phemsi-a          #+#    #+#             */
-/*   Updated: 2021/02/18 00:16:29 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2021/02/18 20:19:04 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,29 +31,35 @@
 ** Your read must use the BUFFER_SIZE defined during compilation to read from
 ** a file or from stdin
 */
-// ! esse parâmetro **line já veio mallocado ou preciso mallocar?
 int	get_next_line(int fd, char **line)
 {
+	char	*line_str;
 	char	*str_read;
 	char	*end_line_ptr;
 	int		read_return;
 
-	if (!(line) || !(str_read = (char*)calloc((BUFFER_SIZE + 1), sizeof(char))))
+	line_str = *line;
+	if (!(line_str) || !(str_read = (char*)calloc((BUFFER_SIZE + 1), sizeof(char))))
 		return (-1);
 	read_return = read(fd, str_read, BUFFER_SIZE);
+	// printf("read_return: %d\n", read_return);
 	while ((end_line_ptr = ft_strchr(str_read, '\n')) == NULL)
 	{
 		if ((read_return == -1) || (read_return == 0))
 		{
 			free(str_read);
-			free(*line);
+			free(line_str);
 			return (read_return);
 		}
 		str_read[BUFFER_SIZE] = '\0';
-		*line = ft_strjoin(*line, str_read);
+		line_str = ft_strjoin(line_str, str_read);
+		// printf("line_str: %s\n", line_str);
 		read_return = read(fd, str_read, BUFFER_SIZE);
+		// printf("read_return: %d\n", read_return);
 	}
 	*end_line_ptr = '\0';
-	*line = ft_strjoin(*line, str_read);
+	line_str = ft_strjoin(line_str, str_read);
+	// printf("line finale: %s\n", line_str);
+	*line = line_str;
 	return (1);
 }
